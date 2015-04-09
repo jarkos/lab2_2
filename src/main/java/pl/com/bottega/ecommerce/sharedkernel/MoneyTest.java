7b0da7715470ad5370ee80d188cddebb3a42bb8c
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-04-09 18:50:45 +0200

    Refactor names of tests.

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.CoreMatchers.*;
 public class MoneyTest {
 
 	@Test
-	public void testAdd10PLNand20PLN_sholudEquals30PLN_resultTrue() {
+	public void testAdd10PLNand20PLN_sholudEquals30PLN_resultShouldBeTrue() {
 		Money money1 = new Money(10, "PLN");
 		Money money2 = new Money(20, "PLN");
 		Money result = new Money(30, "PLN");
@@ -15,14 +15,14 @@ public class MoneyTest {
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-		public void testAdd10PLNand20GBP_shouldIllegalArgumentExceptionResult() {
+		public void testAdd10PLNand20GBP_shouldThrowIllegalArgumentException() {
 			Money money1 = new Money(10, "PLN");
 			Money money2 = new Money(20, "GBP");
 			money1.add(money2);		
 		}
 	
 	@Test
-	public void testSubstract30PLNand10PLN_sholudEquals20PLN_resultTrue() {
+	public void testSubstract30PLNand10PLN_sholudEquals20PLN_resultShouldBeTrue() {
 		Money money1 = new Money(30, "PLN");
 		Money money2 = new Money(10, "PLN");
 		Money result = new Money(20, "PLN");
@@ -37,7 +37,7 @@ public class MoneyTest {
 		}
 	
 	@Test
-		public void test10PLNmultiplyBy10_sholudReturn100PLN_resultTrue() {
+		public void test10PLNmultiplyBy10_sholudReturn100PLN_resultShouldBeTrue() {
 			Money money1 = new Money(10, "PLN");
 			Money result = new Money(100, "PLN");
 			assertThat(money1.multiplyBy(10), is(equalTo(result)));
@@ -51,28 +51,28 @@ public class MoneyTest {
 	}
 	
 	@Test
-		public void test10PLNlessThan100PLN_shouldResultTrue() {
+		public void test10PLNlessThan100PLN_ResultShouldBeTrue() {
 			Money money1 = new Money(10.00, "PLN");
 			Money money2 = new Money(100.00, "PLN");
 			assertThat(money1.lessThan(money2), is(true));
 		}
 	
 	@Test
-	public void test99PLNgreaterThan100PLN_shouldResultFalse() {
+	public void test99PLNgreaterThan100PLN_ResultShouldBeFalse() {
 		Money money1 = new Money(99, "PLN");
 		Money money2 = new Money(100, "PLN");
 		assertThat(money1.greaterThan(money2), is(false));
 	}
 	
 	@Test
-	public void test100PLNequals100PLN_shouldResultTrue() {
+	public void test100PLNequals100PLN_ResultShouldBeTrue() {
 		Money money1 = new Money(100, "PLN");
 		Money money2 = new Money(100, "PLN");
 		assertThat(money1.equals(money2), is(true));
 	}
 	
 	@Test
-	public void test100PLNlessOrEquals100PLN_shouldResultTrue() {
+	public void test100PLNlessOrEquals100PLN_ResultShouldBeTrue() {
 		Money money1 = new Money(99, "PLN");
 		Money money2 = new Money(100, "PLN");
 		assertThat(money1.lessOrEquals(money2), is(true));
